bb2196ccb29aada34604534baec2eab7ea494595
Arturkorol <51499076+Arturkorol@users.noreply.github.com>
2025-08-09 15:49:13 +1000

    Update main.py

--- bot_alista/main.py
+++ bot_alista/main.py
@@ -1,7 +1,10 @@
 """Entry point for running the Telegram bot."""
 
 import asyncio
+import sys
+import os
 
+sys.path.append(os.path.dirname(__file__))
 from bot_alista.bot import main as run_bot
 
 
