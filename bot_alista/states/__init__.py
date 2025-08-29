c61d7354839d420ffe16fbaa2179714646102587
Arturkorol <51499076+Arturkorol@users.noreply.github.com>
2025-08-29 14:16:24 +1000

    Delete bot_alista directory

--- bot_alista/states/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class RequestStates(StatesGroup):
-    request_name = State()
-    request_car = State()
-    request_contact = State()
-    request_price = State()
-    request_comment = State()
